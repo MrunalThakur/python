ee58224513f3dcc80082c2ca18ff66fc77956263
msthakur <msthakur.signups@gmail.com>
2023-04-13 01:18:45 +0530

    Classes objects and inheritence

---
+++ Student.py
@@ -0,0 +1,7 @@
+class Student: 
+    def __init__(self,name,rollNo,course,gpa,internship):
+        self.name = name
+        self.rollno = rollNo
+        self.course = course
+        self.gpa = gpa
+        self.internship = internship
